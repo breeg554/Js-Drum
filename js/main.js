8f1e6ed68979a53235aa9b8c7b2e1d7d51e3f391
breeg554 <you@example.com>
2019-03-27 16:03:04 +0100

    zmiana funkcji js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-window.addEventListener("keydown",function(e){
+const keys = document.querySelectorAll('.key');
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key_div = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
     audio.currentTime = 0;
     audio.play();
     key_div.classList.add('active');
-});
+};
+function removeTran(e){
+    // if(e.propertyName !== 'transfrom') return;
+    this.classList.remove('active');
+}
 
-
-window.addEventListener("keyup",function(e){
-    const key_div = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    key_div.classList.remove('active');
-});
+window.addEventListener("keydown",playSound);
+keys.forEach(key=>key.addEventListener('transitionend',removeTran));
